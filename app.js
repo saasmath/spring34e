104f12debd68829e692f7631d4873862f4b7c933
Gary Anderson <ganderson@seattleacademy.org>
2015-05-19 14:03:10 -0700

    add index.js geometry module and app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var geometry = require('./index.js');
+console.log( 'The area of a circle of radius 4 is '
+           + geometry.circlearea(4));
+
+console.log( 'The circumference of a circle of radius 4 is',
+            geometry.circlecircumference(4));
+
+console.log( 'The area of a square with side 4 is',
+            geometry.squarearea(4));
---
+++ index.js
@@ -0,0 +1,13 @@
+var PI = Math.PI;
+
+exports.circlearea = function (r) {
+  return PI * r * r;
+};
+
+exports.circlecircumference = function (r) {
+  return 2 * PI * r;
+};
+
+exports.squarearea = function (s) {
+  return s * s;
+};
